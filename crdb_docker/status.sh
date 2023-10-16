55732d1424e58ae649be4a77563de6c00b7258f4
yunchi <13524919136@163.com>
2023-10-16 19:25:06 -0400

    debug status

--- crdb_docker/status.sh
+++ crdb_docker/status.sh
@@ -1,2 +1,2 @@
 roach_id=${1:-1}
-docker exec -it roach0 ./cockroach node status --host=roach$node_id:26257 --insecure
+docker exec -it roach0 ./cockroach node status --host=roach$roach_id:26257 --insecure
