200831df46a27195731ebdeea3c92c5b0dae3982
yunchi <13524919136@163.com>
2023-10-16 19:25:36 -0400

    tolerate errors in ycsb run

--- crdb_docker/ycsb.sh
+++ crdb_docker/ycsb.sh
@@ -16,7 +16,7 @@ if [ $should_load == 1 ]; then
     docker exec -it roach0 \
         ./cockroach workload init ycsb \
         --drop \
-        postgresql://root@roach1:26257:26257?sslmode=disable
+        postgresql://root@roach1:26257?sslmode=disable
 fi
 
 if [ $should_run == 1 ]; then
@@ -24,10 +24,11 @@ if [ $should_run == 1 ]; then
         ./cockroach workload run ycsb \
         postgresql://root@roach1:26257,roach2:26257,roach3:26257?sslmode=disable \
         --workload=A \
-        --max-ops=10000 \
-        --duration=10s \
+        --max-ops=1000000 \
+        --duration=10m \
         --max-rate=0 \
         --display-every=1s \
+        --tolerate-errors \
         # > ${log_dir}/raw.log 2> >(tee ${log_dir}/runtime.log >&2)
 fi
 
