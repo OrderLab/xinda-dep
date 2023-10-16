f1f1aa58b48c1d20c34c859f3cc5afe3e2ca36ea
yunchi <13524919136@163.com>
2023-10-16 17:58:03 -0400

    ycsb workload support incomplete clusters

---
+++ crdb_docker/ycsb.sh
@@ -0,0 +1,33 @@
+# PARAMETERS
+log_dir=~/data/ycsb
+
+should_load=0
+should_run=0
+while [ $# -gt 0 ]; do
+    case "$1" in
+    "load") should_load=1;;
+    "run") should_run=1;;
+    esac
+    shift
+done
+
+
+if [ $should_load == 1 ]; then
+    docker exec -it roach0 \
+        ./cockroach workload init ycsb \
+        --drop \
+        postgresql://root@roach1:26257:26257?sslmode=disable
+fi
+
+if [ $should_run == 1 ]; then
+    docker exec -it roach0 \
+        ./cockroach workload run ycsb \
+        postgresql://root@roach1:26257,roach2:26257,roach3:26257?sslmode=disable \
+        --workload=A \
+        --max-ops=10000 \
+        --duration=10s \
+        --max-rate=0 \
+        --display-every=1s \
+        # > ${log_dir}/raw.log 2> >(tee ${log_dir}/runtime.log >&2)
+fi
+
